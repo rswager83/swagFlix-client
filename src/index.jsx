2c76fd4de2e24a5f1a5b93cebaa006daba41e931
rswager83 <101044506+rswager83@users.noreply.github.com>
2022-06-17 11:33:31 +0200

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MainView } from './components/main-view/main-view';
 
     // Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
@@ -8,9 +9,7 @@ import './index.scss';
 class SwagFlixApplication extends React.Component {
     render() {
         return (
-            <div className='Swag-Flix'>
-                <div>Good Morning Sunshine!</div>
-            </div>
+            <MainView />
         );
     }
 }
