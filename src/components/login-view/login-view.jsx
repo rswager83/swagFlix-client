7677cd0c8dfa4c4207dd325cb9e78582d0e63a3f
rswager83 <101044506+rswager83@users.noreply.github.com>
2022-06-24 15:54:34 +0200

    Update login-view.jsx
    Incorporating Bootstrap

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,35 +1,56 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { Form, Button } from "react-bootstrap";
+
+import "./login-view.scss";
 
 export function LoginView(props) {
-    const [ username, setUsername ] = useState('');
-    const [ password, setPassword ] = useState('');
-    
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(username, password);
-        props.onLoggedIn(username);
-    };
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    return (
-        <form>
-            <label>
-                Username:
-                <input type='text' value={username} onChange={e => setUsername(e.target.value)} />
-            </label>
-            <label>
-                Password:
-                <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
-            </label>
-            <button type='submit' onClick={handleSubmit}>Submit</button>
-        </form>
-    );
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(username, password);
+    props.onLoggedIn(username);
+  };
+
+  return (
+    <Form>
+      <h2 className="mb-3 mx-auto mt-5">Login to SwagFlix</h2>
+      <Form.Group className="mb-3 mx-auto mt-4" controlId="formUsername">
+        <Form.Label>Username:</Form.Label>
+        <Form.Control
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+          placeholder="Enter a username"
+        />
+      </Form.Group>
+
+      <Form.Group className="mb-3 mx-auto mt-4">
+        <Form.Label>Password:</Form.Label>
+        <Form.Control
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+          minLength="8"
+          placeholder=""
+        />
+      </Form.Group>
+
+      <Button className="mt-4" type="submit" onClick={handleSubmit}>
+        Submit
+      </Button>
+    </Form>
+  );
 }
 
 LoginView.PropTypes = {
-    user: PropTypes.shape({
-        username: PropTypes.string.isRequired,
-        password: PropTypes.string.isRequired
-    }),
-    onLoggedIn: PropTypes.func.isRequired,
+  user: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    password: PropTypes.string.isRequired,
+  }),
+  onLoggedIn: PropTypes.func.isRequired,
 };
