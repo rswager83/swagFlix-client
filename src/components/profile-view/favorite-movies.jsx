226477a3b8f45856977c08ee635a6b610a8a68b3
rswager83 <101044506+rswager83@users.noreply.github.com>
2022-06-30 19:32:00 +0200

    Update favorite-movies.jsx
    Haven't decided yet if I will use this as a component

--- src/components/profile-view/favorite-movies.jsx
+++ src/components/profile-view/favorite-movies.jsx
@@ -5,52 +5,42 @@ import { Col, Row, Button, Figure, Card } from "react-bootstrap";
 import "./profile-view.scss";
 
 export function FavoriteMovieView(props) {
-  const { movies, favoriteMovies, currentUser, token } = props;
+  const { movies, favoriteMovies, currentUser } = props;
 
   const favoriteMoviesId = favoriteMovies.map((m) => m._id);
 
-  const favoriteMovieList = movies.filter((m) => {
+  const favoriteMoviesList = movies.filter((m) => {
     return favoriteMoviesId.includes(m._id);
   });
 
-  const handleMovieDelete = (id) => {
-    let token = localStorage.getItem("token");
-
-    axios
-      .delete(
-        `https://swagflix.herokuapp.com/users/${localStorage.getItem(
-          "user"
-        )}/movies/${id}`,
-        {
-          headers: { Authorization: `Bearer ${token}` },
-        }
-      )
-      .then(() => {
-        alert("The movie was successfully removed from list.");
-        window.open("/users/:username", "_self");
-      })
-      .catch((error) => console.error(error));
-  };
+  const [favoriteMoviesList, setFavoriteMoviesList] = useState([]);
 
   return (
     <Card>
       <Card.Body>
         <Row>
           <Col xs={12}>
-            <h3>FavoriteMovies</h3>
+            <h3>I am here</h3>
           </Col>
         </Row>
+
         <Row>
-          {favoriteMovieList.map(({ ImagePath, Title, _id }) => {
+          {favoriteMoviesList.map((movie) => {
             return (
               <Col xs={12} md={6} lg={3} key={_id} className="fav-movie">
                 <Figure>
-                  <Link to={`/movies/${_id}`}>
-                    <Figure.Image src={ImagePath} alt={Title} />
-                    <Figure.Caption>{Title}</Figure.Caption>
+                  <Link to={`/movies/${movie._id}`}>
+                    <Figure.Image src={movie.ImagePath} alt={movie.Title} />
+                    <Figure.Caption>{movie.Title}</Figure.Caption>
                   </Link>
                 </Figure>
-                <Button onClick={() => removeFav(_id)}>Remove from list</Button>
+                <Button
+                  onClick={() => {
+                    handleMovieDelete(_id);
+                  }}
+                >
+                  Remove from list
+                </Button>
               </Col>
             );
           })}
