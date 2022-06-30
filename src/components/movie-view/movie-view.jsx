eb431c3b2de752b8e9ee0406ac0d13bab9fee9ed
rswager83 <101044506+rswager83@users.noreply.github.com>
2022-06-30 19:32:54 +0200

    Update movie-view.jsx
    Adding addMovie, delFavMovie functions and their buttons

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import axios from "axios";
 
 import { Link } from "react-router-dom";
 import { Button, Container, Row, Col } from "react-bootstrap";
@@ -7,16 +8,59 @@ import { Button, Container, Row, Col } from "react-bootstrap";
 import "./movie-view.scss";
 
 export class MovieView extends React.Component {
-  keypressCallback(event) {
-    console.log(event.key);
+  constructor() {
+    super();
+
+    this.state = {
+      movies: [],
+      user: null,
+    };
   }
 
-  componentDidMount() {
-    document.addEventListener("keypress", this.keypressCallback);
+  addMovie(movie, user) {
+    let username = localStorage.getItem("user");
+    let token = localStorage.getItem("token");
+    console.log(movie);
+    console.log(token);
+
+    axios
+      .post(
+        `https://swagflix.herokuapp.com/users/${username}/movies/${movie._id}`,
+        {},
+        { headers: { Authorization: `Bearer ${token}` } }
+      )
+      .then((response) => {
+        console.log(response.data);
+        alert(`${movie.Title} has been added from your list.`);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
+
+  delFavMovie = (movie, user) => {
+    let token = localStorage.getItem("token");
+    let username = localStorage.getItem("user");
+    console.log(movie);
+    console.log(token);
+    axios
+      .delete(
+        `https://swagflix.herokuapp.com/users/${username}/movies/${movie._id}`,
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      )
+      .then((response) => {
+        console.log(response.data);
+        alert(`${movie.Title} has been removed from your list.`);
+      })
+      .catch((e) => {
+        console.log("Error");
+      });
+  };
 
   render() {
-    const { movie, onBackClick } = this.props;
+    const { movie, user, onBackClick } = this.props;
 
     return (
       <Container className="movie-view">
@@ -42,12 +86,28 @@ export class MovieView extends React.Component {
           </Link>
         </Row>
         <Button
-          className="mt-4"
+          className=""
           onClick={() => {
             onBackClick(null);
           }}
         >
           Back
+        </Button>
+        <Button
+          className="button ml-2"
+          onClick={() => {
+            this.addMovie(movie, user);
+          }}
+        >
+          Add to favorites
+        </Button>
+        <Button
+          className="button ml-2"
+          onClick={() => {
+            this.delFavMovie(movie, user);
+          }}
+        >
+          Remove from favorites
         </Button>
       </Container>
     );
