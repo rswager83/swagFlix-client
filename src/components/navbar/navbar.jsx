cb40a43e46034ef68b86d6d9a22d800cc7e64edf
rswager83 <101044506+rswager83@users.noreply.github.com>
2022-06-30 19:26:08 +0200

    Update navbar.jsx

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -22,27 +22,21 @@ export function Menu({ user }) {
   };
 
   return (
-    <Navbar
-      className="main-nav"
-      style={{ width: "100vh" }}
-      sticky="top"
-      expand="lg"
-    >
-      <Container>
-        <Navbar.Brand className="text-light">SwagFlix</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            {isAuth() && <Nav.Link href="/">Home</Nav.Link>}
-            {isAuth() && <Nav.Link href={`/users/${user}`}>{user}</Nav.Link>}
+    <Navbar className="main-nav" expand="lg">
+      {/* <Container> */}
+      <Navbar.Brand className="text-light">SwagFlix</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          {isAuth() && <Nav.Link href="/">Home</Nav.Link>}
+          {isAuth() && <Nav.Link href={`/users/${user}`}>{user}</Nav.Link>}
 
-            {isAuth() && <Button onClick={onLoggedOut}>Logout</Button>}
-            {!isAuth() && <Nav.Link href="/">Sign-in</Nav.Link>}
-            {!isAuth() && <Nav.Link href="/register">Sign-up</Nav.Link>}
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+          {isAuth() && <Button onClick={onLoggedOut}>Logout</Button>}
+          {!isAuth() && <Nav.Link href="/">Sign-in</Nav.Link>}
+          {!isAuth() && <Nav.Link href="/register">Sign-up</Nav.Link>}
+        </Nav>
+      </Navbar.Collapse>
+      {/* </Container> */}
     </Navbar>
   );
 }
-// to={`/users/${user}`}
