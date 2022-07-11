c7549ece240bccfc8cbf47c86567f009bd3f2c1d
rswager83 <101044506+rswager83@users.noreply.github.com>
2022-07-11 09:57:33 +0200

    Update index.jsx
    Creating store for app

--- src/index.jsx
+++ src/index.jsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Container from 'react-bootstrap/Container';
-import { MainView } from './components/main-view/main-view';
+import React from "react";
+import { createRoot } from "react-dom/client";
+// import { ReactDOM } from "react-dom";
+// import Container from "react-bootstrap/Container";
+import MainView from "./components/main-view/main-view";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import moviesApp from "./reducers/reducers";
+import { devToolsEnhancer } from "redux-devtools-extension";
 
-    // Import statement to indicate that you need to bundle `./index.scss`
-import './index.scss';
+// Import statement to indicate that you need to bundle `./index.scss`
+import "./index.scss";
 
-    // Main component (will eventually use all the others)
+const store = createStore(moviesApp, devToolsEnhancer());
+
+// Main component (will eventually use all the others)
 class SwagFlixApplication extends React.Component {
-    render() {
-        return (
-            <Container>
-                <MainView />
-            </Container>
-        );
-    }
+  render() {
+    return (
+      <Provider store={store}>
+        <MainView />
+      </Provider>
+    );
+  }
 }
 
-    // Finds the root of your app
-const container = document.getElementsByClassName('app-container') [0];
-
-    // Tells React to render your app in the root DOM element
-ReactDOM.render(React.createElement(SwagFlixApplication), container);
-
+// Finds the root of your app
+const container = document.getElementsByClassName("app-container")[0];
+const root = createRoot(container);
+// Tells React to render your app in the root DOM element
+root.render(React.createElement(SwagFlixApplication));
