51f9b3d8c848f43900af889bc91eb65da762f24a
rswager83 <101044506+rswager83@users.noreply.github.com>
2022-07-18 10:34:58 +0200

    Update movies-list.jsx
    Minor editing

--- src/components/movies-list/movies-list.jsx
+++ src/components/movies-list/movies-list.jsx
@@ -23,18 +23,18 @@ function MoviesList(props) {
 
   if (!movies) return <div className="main-view" />;
 
-  return;
-  <>
-    <Col md={12} style={{ margin: "1em" }}>
-      <VisibilityFilterInput visibilityFilter={visibilityFilter} />
-    </Col>
-    {filteredMovies.map((m) => (
-      <Col md={3} key={m._id}>
-        <MovieCard movie={m} />
+  return (
+    <>
+      <Col md={12} style={{ margin: "1em" }}>
+        <VisibilityFilterInput visibilityFilter={visibilityFilter} />
       </Col>
-    ))}
-    ;
-  </>;
+      {filteredMovies.map((m) => (
+        <Col md={3} key={m._id}>
+          <MovieCard movie={m} />
+        </Col>
+      ))}
+    </>
+  );
 }
 
 MoviesList.propTypes = {
